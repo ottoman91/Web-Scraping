8b15815f647652330a8ae356995dca1f93a2e394
Usman Khaliq <usmankhaliq@Usmans-MacBook-Pro.local>
2015-08-26 09:23:10 +0500

    getting all URLs of the states

--- get_state_links.py
+++ get_state_links.py
@@ -8,30 +8,32 @@ data = [] # for storing school links and displaying them
 
 
 #function for getting the URL for states data on schools
-def get_state_links(BASE_URL):
+def get_state_links(BASE_URL): 
+	STATE_LINKS = []
 	html = urlopen(BASE_URL).read()
 	soup = BeautifulSoup(html, "lxml")
-	state_list = soup.find("ul","tab-list tab-list-short")
-	state_links = [li.a["href"] for li in state_list.findAll("li")] 
-	return state_links 
-
-#data.append(get_state_links(BASE_URL)) #for testing
-#print data #for testing
+	state_list = soup.find_all("ul","tab-list tab-list-short") 
+	for list in state_list: 
+		state_links = [li.a["href"] for li in list.findAll("li")] 
+		STATE_LINKS.append(state_links) 
+	return STATE_LINKS 
+data.append(get_state_links(BASE_URL)) #for testing
+print data #for testing
 	
-def get_school_info(state_url):
-	html = urlopen(state_url).read()
-	soup = BeautifulSoup(html,"lxml")
-	state = soup.find("h1","city").string  
-	school_data_table = soup.find("div","table-responsive") 
-	school_info = [td.string for td in school_data_table.findAll("td")]
-	return school_info 
+#def get_school_info(state_url):
+#	html = urlopen(state_url).read()
+#	soup = BeautifulSoup(html,"lxml")
+#	state = soup.find("h1","city").string  
+#	school_data_table = soup.find("div","table-responsive") 
+#	school_info = [td.string for td in school_data_table.findAll("td")]
+#	return (school_info,state) 
 
-state_info = get_state_links(BASE_URL) 
+#state_info = get_state_links(BASE_URL) 
 
-for info_link in state_info:
-	school_infos = get_school_info(info_link) 
-	data.append(school_infos)
+#for info_link in state_info:
+#	school_infos,state = get_school_info(info_link) 
+#	data.append(state)
 	
 
-print data
+#print data
 
